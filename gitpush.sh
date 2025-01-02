cbcf62dea0ed34031c23608f7551ccd616be6b6d
adarsh-iiit <adarshtech251@gmail.com>
2025-01-02 18:16:52 +0530

    first commit

---
+++ gitpush.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Prompt user for commit message and branch name
+read -p "Enter commit message and branch in the format 'message / branch': " input
+
+# Extract message and branch name using the '==' delimiter
+commit_message=$(echo "$input" | awk -F '/' '{print $1}')
+branch_name=$(echo "$input" | awk -F '/' '{print $2}')
+
+# Check if both message and branch name are provided
+if [ -z "$commit_message" ] || [ -z "$branch_name" ]; then
+    echo "Error: Please provide both a commit message and branch name in the format 'message  /  branch'."
+    exit 1
+fi
+
+# Check if the current directory is a git repository
+if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    # Execute the Git commands
+    git add .
+    git commit -m "$commit_message"
+    git push -u origin "$branch_name"
+    echo "Changes pushed to branch: $branch_name"
+else
+    echo "Error: This is not a Git repository."
+fi
+
+
